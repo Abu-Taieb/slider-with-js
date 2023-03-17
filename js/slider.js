96624bb7573ae0f49e86380c6388ba3780300de2
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-03-17 20:22:55 +0600

    slider design

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,27 @@
-let imgArray = [
-  "images/pic-1.jpg",
-  "images/pic-2.jpg",
-  "images/pic-3.jpg",
-  "images/pic-4.jpg",
-  "images/pic-5.jpg",
-  "images/pic-6.jpg",
-  "images/pic-7.jpg",
-  "images/pic-8.jpg",
-  "images/pic-9.jpg",
+// image array 
+let images = [
+    'images/pic-1.jpg',
+    'images/pic-2.jpg',
+    'images/pic-3.jpg',
+    'images/pic-4.jpg',
+    'images/pic-5.jpg',
+    'images/pic-6.jpg',
+    'images/pic-7.jpg',
+    'images/pic-8.jpg',
+    'images/pic-9.jpg',
 ];
 
-const getId = document.getElementById("image-carousel");
+// get image id 
+const imgId = document.getElementById('image-carousel');
 
-let imgIndex = 0;
-setInterval(() => {
-  if (imgIndex === imgArray.length) {
-    imgIndex = 0;
-  }
-
-  let imgLink = imgArray[imgIndex];
-  getId.setAttribute("src", imgLink);
-  imgIndex++;
-}, 500);
+let index = 0;
+setInterval( () => {
+    // condition 
+    if(index === images.length){
+        index = 0;}
+    
+    let image = images[index];
+    index++;
+    // set image 
+    imgId.setAttribute('src', image)
+},1000)
